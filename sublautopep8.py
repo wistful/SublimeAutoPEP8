ee620e84cd6e48af48f5e09dfa4f373da5201d26
wistful <wst.public.mail@gmail.com>
2012-10-20 15:44:03 +0300

    closed pull request #2

--- sublautopep8.py
+++ sublautopep8.py
@@ -18,22 +18,26 @@ class AutoPep8PreviewCommand(sublime_plugin.TextCommand):
             params.append("--select=" + settings.get("select"))
 
         p = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=plugin_path)
-        diff = p.stdout.read()
+        encoding = sublime.active_window().active_view().encoding()
+        diff = p.stdout.read().decode(encoding)
         for line in p.stderr:
             print line
         if len(diff) == 0:
             sublime.message_dialog("0 issues to fix")
         else:
             sublime.active_window().new_file()
+            sublime.active_window().active_view().set_encoding(encoding)
             sublime.active_window().active_view().set_syntax_file("Packages/Diff/Diff.tmLanguage")
             sublime.active_window().active_view().insert(edit, 0, diff)
 
 
 class AutoPep8Command(sublime_plugin.TextCommand):
     def run(self, edit):
+        encoding = sublime.active_window().active_view().encoding()
         plugin_path = os.path.join(sublime.packages_path(), "AutoPEP8")
         fd, tmp_path = tempfile.mkstemp()
-        open(tmp_path, 'w').write(self.view.substr(sublime.Region(0, self.view.size())))
+        data = self.view.substr(sublime.Region(0, self.view.size()))
+        open(tmp_path, 'w').write(data.encode(encoding))
         params = ["python", "autopep8.py", tmp_path, "-i"]
         settings = sublime.load_settings('AutoPep8.sublime-settings')
         if settings.get("ignore"):
@@ -44,4 +48,5 @@ class AutoPep8Command(sublime_plugin.TextCommand):
         p = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=plugin_path)
         for line in p.stderr:
             print line
-        self.view.replace(edit, sublime.Region(0, self.view.size()), open(tmp_path, 'r').read())
+        self.view.replace(edit, sublime.Region(0, self.view.size()), open(tmp_path, 'r').read().decode(encoding))
+
