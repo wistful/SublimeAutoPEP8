492300199899d6b2eadbd69f759312821be234c2
wistful <wst.public.mail@gmail.com>
2012-10-24 13:34:31 +0300

    use config to specify 'python' and 'autopep8'

--- sublautopep8.py
+++ sublautopep8.py
@@ -7,16 +7,17 @@ import os
 
 plugin_path = os.path.split(os.path.abspath(__file__))[0]
 
+
 class AutoPep8PreviewCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        file_path = sublime.active_window().active_view().file_name()
-        params = ["env", "python", "autopep8.py", file_path, "-d", "-vv"]
         settings = sublime.load_settings('AutoPep8.sublime-settings')
+        file_path = sublime.active_window().active_view().file_name()
+        params = [settings.get("python", "python"), settings.get("autopep8", "autopep8.py"), file_path, "-d", "-vv"]
         if settings.get("ignore"):
             params.append("--ignore=" + settings.get("ignore"))
         if settings.get("select"):
             params.append("--select=" + settings.get("select"))
-
+        print 'autopep8:', params
         p = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=plugin_path)
         encoding = sublime.active_window().active_view().encoding()
         diff = p.stdout.read().decode(encoding)
@@ -33,19 +34,18 @@ class AutoPep8PreviewCommand(sublime_plugin.TextCommand):
 
 class AutoPep8Command(sublime_plugin.TextCommand):
     def run(self, edit):
+        settings = sublime.load_settings('AutoPep8.sublime-settings')
         encoding = sublime.active_window().active_view().encoding()
         fd, tmp_path = tempfile.mkstemp()
         data = self.view.substr(sublime.Region(0, self.view.size()))
         open(tmp_path, 'w').write(data.encode(encoding))
-        params = ["python", "autopep8.py", tmp_path, "-i"]
-        settings = sublime.load_settings('AutoPep8.sublime-settings')
+        params = [settings.get("python", "python"), settings.get("autopep8", "autopep8.py"), tmp_path, "-i"]
         if settings.get("ignore"):
             params.append("--ignore=" + settings.get("ignore"))
         if settings.get("select"):
             params.append("--select=" + settings.get("select"))
-
+        print 'autopep8:', params
         p = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=plugin_path)
         for line in p.stderr:
             print line
         self.view.replace(edit, sublime.Region(0, self.view.size()), open(tmp_path, 'r').read().decode(encoding))
-
