f55d72d3447e05a92ad5988886daddef5de0e674
wistful <wst.public.mail@gmail.com>
2017-03-27 20:22:43 -0700

    Update version to 1.3.4

--- sublautopep8.py
+++ sublautopep8.py
@@ -19,7 +19,7 @@ try:
 except NameError:
     unicode = str
 
-VERSION = '1.3.4-rc1'
+VERSION = '1.3.4'
 
 logger = logging.getLogger('SublimeAutoPEP8')
 logger.addHandler(logging.StreamHandler(sys.stdout))
